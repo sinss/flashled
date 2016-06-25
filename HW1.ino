e2b80c5fa05ecf72bfd92c5ba89fb1c58e8b524b
Leo Chang <sinss0000@gmail.com>
2016-06-25 18:23:51 +0800

    Arduino home work
    HW1

---
+++ HW1.ino
@@ -0,0 +1,29 @@
+int led[] = {10,11,12,13};
+int i;
+int j=0;
+bool reverse = false;
+void setup() {    
+  for(i=0;i<4;i++)
+  {  
+    pinMode(led[i], OUTPUT); 
+  }    
+}
+
+void loop() {
+  for(i=0;i<4;i++)
+  {
+    digitalWrite(led[i],LOW);
+  }  
+  digitalWrite(led[j], HIGH); 
+  delay(200);
+  if (reverse == true) {
+    j--;
+  } else {
+    j++;
+  }
+  if(j==3)
+    reverse = true;
+  else if (j== 0) {
+    reverse = false;
+  }
+}
